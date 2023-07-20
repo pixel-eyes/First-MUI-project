cf89b246e8a628f98252d6a1f7d27f8e0c567c02
FIRST_NAME LAST_NAMEr-xxxl  git config --global user.name r-xxxlgit config --global user.name FIRST_NAME <rachel-x.li@arup.com>
2023-07-20 15:02:29 +1000

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
   Container,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
+import { Button } from '@material-ui/core';
+
 
 function App() {
   return (
@@ -26,7 +28,27 @@ function App() {
       <main>
         <div>
           <Container maxWidth="sm">
-            <Typography variant="h2" align="center" color=""></Typography>
+            <Typography variant="h2" align="center" color="textPrimay" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragrah>
+              Hello everyone this is a photo album
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                    </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary section
+                    </Button>
+                </Grid>
+
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
